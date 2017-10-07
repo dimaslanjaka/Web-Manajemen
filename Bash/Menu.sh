cee8b5442c23eef9b1f635cea4d31214dd8319e9
dimaslanjaka <dimaslanjaka@gmail.com>
2017-10-07 16:57:16 +0700

    Update Menu.sh

--- Bash/Menu.sh
+++ Bash/Menu.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ] then
- echo "Please Run As Root"
- exit
+# Init
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+#....
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
 fi
 EDITOR=vim
 PASSWD=/etc/passwd
