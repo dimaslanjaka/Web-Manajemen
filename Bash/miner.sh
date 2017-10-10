8200ce52c627862dc1e0e24ebdb5eeea161b723e
dimaslanjaka <dimaslanjaka@gmail.com>
2017-10-11 02:43:57 +0700

    Create miner.sh

---
+++ Bash/miner.sh
@@ -0,0 +1,6 @@
+if pgrep -x "miner" > /dev/null
+then
+    echo "Miner Is Running"
+else
+    /home/ubuntu/cpuminer-multi/minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u candrarisky1922@gmail.com -p x -t 1
+fi
