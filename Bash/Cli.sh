e1b765c08a67e8d461b71a3e66dfacbb87267dab
dimaslanjaka <dimaslanjaka@gmail.com>
2017-10-11 02:35:02 +0700

    Update Cli.sh

--- Bash/Cli.sh
+++ Bash/Cli.sh
@@ -3,5 +3,5 @@ if pgrep -x "minerd" > /dev/null
 then
     echo "Miner Is Running"
 else
-    minergate-cli -user dimaslanjaka@gmail.com -xmr 1
+    minergate-cli -user candrarisky1922@gmail.com -xmr 1
 fi
