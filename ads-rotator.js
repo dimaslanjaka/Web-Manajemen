0bb86fd34fb17659e10c14b261c5e74bde4c6ba7
dimaslanjaka <dimaslanjaka@gmail.com>
2017-11-23 14:41:13 +0700

    Update ads-rotator.js

--- ads-rotator.js
+++ ads-rotator.js
@@ -6,7 +6,7 @@ var jsad = '<sc'+'ript async src="//pagead2.googlesyndication.com/pagead/js/adsb
 $('#adsense,.adsense').html(adsense);$('#matched,.matched').html(matched);
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = 'http://pagead2.googlesyndication.com/pagead/show_ads.js';
+script.src = '//pagead2.googlesyndication.com/pagead/show_ads.js';
 document.body.appendChild(script);
 $('#related-summary').html('');
 $('#related-summary').append(matched);
