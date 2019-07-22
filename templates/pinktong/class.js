1052beb058b1a2198fffc340b61c47285609b005
dimaslanjaka <dimaslanjaka@gmail.com>
2019-07-22 21:43:47 +0700

    +class

---
+++ templates/pinktong/class.js
@@ -0,0 +1,9 @@
+String.prototype.CSS = function() {
+  var l = document.createElement('link');
+  l.rel = 'stylesheet';
+  l.href = this;
+  var h = document.getElementsByTagName('head')[0], event = window.addEventListener || window.attachEvent || window.onload;
+  event('load', function(){
+    return h.parentNode.insertBefore(l, h);
+  });
+}
