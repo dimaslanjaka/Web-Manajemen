7e4b990bc02b7d02b053925a35272ae9d79be744
dimaslanjaka <dimaslanjaka@gmail.com>
2019-07-23 01:49:55 +0700

    ++js minify

--- templates/pinktong/class.js
+++ templates/pinktong/class.js
@@ -2,8 +2,10 @@ String.prototype.CSS = function() {
   var l = document.createElement('link');
   l.rel = 'stylesheet';
   l.href = this;
-  var h = document.getElementsByTagName('head')[0], event = window.addEventListener || window.attachEvent || window.onload;
-  event('load', function(){
+  var h = document.getElementsByTagName('head')[0],
+    event = window.addEventListener || window.attachEvent || window.onload;
+  event('load', function() {
     return h.parentNode.insertBefore(l, h);
   });
 }
+
