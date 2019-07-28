1b48615486de014056ce4abcf1a7958b2e53764a
dimaslanjaka <dimaslanjaka@gmail.com>
2019-07-28 22:02:38 +0700

    ++fix & translator

--- templates/pinktong/mgid.js
+++ templates/pinktong/mgid.js
@@ -1,5 +1,35 @@
-window.onload = function(){
-            var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById',lp=d.location.protocol,wp=lp.indexOf('http')==0?lp:'https:';
-            var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M445014ScriptRootC397919")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
-            catch(e){var iw=d;var c=d[gi]("M445014ScriptRootC397919");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=397919;c[ac](dv);
-            var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/b/l/blog.akarmas.com.397919.js?t="+D.getYear()+D.getMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);}
+var D = new Date(),
+  d = document,
+  b = 'body',
+  ce = 'createElement',
+  ac = 'appendChild',
+  st = 'style',
+  ds = 'display',
+  n = 'none',
+  gi = 'getElementById',
+  lp = d.location.protocol,
+  wp = lp.indexOf('http') == 0 ? lp : 'https:';
+var i = d[ce]('iframe');
+i[st][ds] = n;
+d[gi]("M445014ScriptRootC397919")[ac](i);
+try {
+  var iw = i.contentWindow.document;
+  iw.open();
+  iw.writeln("<ht" + "ml><bo" + "dy></bo" + "dy></ht" + "ml>");
+  iw.close();
+  var c = iw[b];
+} catch (e) {
+  var iw = d;
+  var c = d[gi]("M445014ScriptRootC397919");
+}
+var dv = iw[ce]('div');
+dv.id = "MG_ID";
+dv[st][ds] = n;
+dv.innerHTML = 397919;
+c[ac](dv);
+var s = iw[ce]('script');
+s.async = 'async';
+s.defer = 'defer';
+s.charset = 'utf-8';
+s.src = wp + "//jsc.mgid.com/b/l/blog.akarmas.com.397919.js?t=" + D.getYear() + D.getMonth() + D.getUTCDate() + D.getUTCHours();
+c[ac](s);
---
+++ templates/pinktong/translator.js
@@ -0,0 +1,10 @@
+dimas.js('//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit', function() {});
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'id',
+    includedLanguages: 'ar,en,es,jv,ko,pa,pt,ru,zh-CN,id',
+    layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+    autoDisplay: false
+  }, 'google_translate_element');
+}
