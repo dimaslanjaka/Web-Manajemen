f35428126578e020c8b44f71762e688bbd5c0003
dimaslanjaka <dimaslanjaka@gmail.com>
2019-07-28 21:40:06 +0700

    ++ads fix

--- templates/pinktong/adsense.js
+++ templates/pinktong/adsense.js
@@ -1,9 +1,8 @@
 if (typeof dimas != 'undefined') {
-  dimas.js('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', adsgo);
-}
-function adsgo() {
-  var ins = document.getElementsByTagName('ins').length;
-  for (let index = 0; index < ins; index++) {
-    (adsbygoogle = window.adsbygoogle || []).push({});
-  }
+  dimas.js('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', function(){
+    var ins = document.getElementsByTagName('ins').length;
+    for (let index = 0; index < ins; index++) {
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  });
 }
