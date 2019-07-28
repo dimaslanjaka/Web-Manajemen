3ee45d551708630f451998bc36a22bf0ec896712
dimaslanjaka <dimaslanjaka@gmail.com>
2019-07-28 21:08:06 +0700

    ++ads

--- templates/pinktong/adsense.js
+++ templates/pinktong/adsense.js
@@ -1,7 +1,7 @@
-if (typeof dimas != 'undefined'){
+if (typeof dimas != 'undefined') {
   dimas.js('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', adsgo);
 }
 function adsgo(x) {
   var ins = document.getElementsByTagName('ins').length;
-  console.log(ins);
+  console.log({ adsC: ins });
 }
