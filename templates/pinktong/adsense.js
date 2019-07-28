bab78d5bdf19485ac6964cad5d8ed34e0b355535
dimaslanjaka <dimaslanjaka@gmail.com>
2019-07-28 21:36:55 +0700

    ++ads

--- templates/pinktong/adsense.js
+++ templates/pinktong/adsense.js
@@ -1,7 +1,7 @@
 if (typeof dimas != 'undefined') {
   dimas.js('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', adsgo);
 }
-function adsgo(x) {
+function adsgo() {
   var ins = document.getElementsByTagName('ins').length;
   for (let index = 0; index < ins; index++) {
     (adsbygoogle = window.adsbygoogle || []).push({});
