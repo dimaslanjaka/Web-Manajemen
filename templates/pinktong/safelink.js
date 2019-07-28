45e971fcb6a2c24b21423f674dbde8fbf16304fe
dimaslanjaka <dimaslanjaka@gmail.com>
2019-07-28 21:44:17 +0700

    ++fix safelink

--- templates/pinktong/safelink.js
+++ templates/pinktong/safelink.js
@@ -1,30 +1,27 @@
-window.onload = function () {
-
-  var x = Up("url") || Up("u");
-  if (location.host != 'translate.googleusercontent.com' && x && "" != x) {
-    location.replace("https://dimaslanjaka.github.io/page/safelink.html?url=" + x);
-  }
-
-  var EXCLUDE = /facebook\.com|twitter\.com|thumblr\.com|blog\.akarmas\.com|web\-manajemen\.blogspot\.com|dimaslanjaka\.github\.io|translate\.google\.com|translate\.googleusercontent\.com|javascript\:void\(0\)/gm,
-    l = document.addEventListener || document.attachEvent;
-  l("mouseover", function (e) {
-    if ("a" == (e.target.nodeName || e.srcElement.nodeName).toLowerCase()) {
-      var t = e.target,
-        a = t.href;
-      t.getAttribute("data-href") || a.match(EXCLUDE) || (t.setAttribute("data-href", a), t.removeAttribute("href"), t.style.textDecoration = "underline", t.style.color = "red")
-    }
-  }), l("click", function (e) {
-    if ("a" == (e.target.nodeName || e.srcElement.nodeName).toLowerCase()) {
-      e.preventDefault();
-      console.log(e);
-      var t = e.target.href || e.target.getAttribute("data-href");
-      return (e.target.host || t).match(EXCLUDE) ? location.href = t : Object.assign(document.createElement("a"), {
-        target: "_blank",
-        href: "https://dimaslanjaka.github.io/page/safelink.html?url=" + btoa(t)
-      }).click(), !1
-    }
-  });
+var x = Up("url") || Up("u");
+if (location.host != 'translate.googleusercontent.com' && x && "" != x) {
+  location.replace("https://dimaslanjaka.github.io/page/safelink.html?url=" + x);
 }
+
+var EXCLUDE = /facebook\.com|twitter\.com|thumblr\.com|blog\.akarmas\.com|web\-manajemen\.blogspot\.com|dimaslanjaka\.github\.io|translate\.google\.com|translate\.googleusercontent\.com|javascript\:void\(0\)/gm,
+  l = document.addEventListener || document.attachEvent;
+l("mouseover", function (e) {
+  if ("a" == (e.target.nodeName || e.srcElement.nodeName).toLowerCase()) {
+    var t = e.target,
+      a = t.href;
+    t.getAttribute("data-href") || a.match(EXCLUDE) || (t.setAttribute("data-href", a), t.removeAttribute("href"), t.style.textDecoration = "underline", t.style.color = "red")
+  }
+}), l("click", function (e) {
+  if ("a" == (e.target.nodeName || e.srcElement.nodeName).toLowerCase()) {
+    e.preventDefault();
+    console.log(e);
+    var t = e.target.href || e.target.getAttribute("data-href");
+    return (e.target.host || t).match(EXCLUDE) ? location.href = t : Object.assign(document.createElement("a"), {
+      target: "_blank",
+      href: "https://dimaslanjaka.github.io/page/safelink.html?url=" + btoa(t)
+    }).click(), !1
+  }
+});
 
 function Up(e, a) {
   a || (a = window.location.href), e = e.replace(/[\[\]]/g, "\\$&");
