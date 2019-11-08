640a91710cdebe4bf5fce7624e8e36a8978ef295
dimaslanjaka <dimaslanjaka@gmail.com>
2019-11-08 16:56:47 +0700

    fix

--- js/urlparam.js
+++ js/urlparam.js
@@ -3,9 +3,13 @@ try {
   var c = url.searchParams.get("c");
   try {
     t = window.atob(c);
-    if (document.getElementById('result_txt')){
-      document.getElementById('result_txt').innerHTML = t;
+    rs = document.getElementById('result_txt');
+    if (rs) {
+      if (window.btoa(t) == c) {
+        rs.innerHTML = t;
+      }
     }
+
   } catch (e) {
     console.log(e);
   }
