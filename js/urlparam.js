6019b5fc298693755e6b4e65b05c8ec0b4166f18
dimaslanjaka <dimaslanjaka@gmail.com>
2019-11-08 17:02:30 +0700

    fx

--- js/urlparam.js
+++ js/urlparam.js
@@ -7,6 +7,18 @@ try {
     if (rs) {
       if (window.btoa(t) == c) {
         rs.innerHTML = t;
+      } else {
+        rs.innerHTML = '<s'+'c'+'r'+'i'+'p'+'t async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></s'+'c'+'r'+'i'+'p'+'t>\
+        <!-- Responsive -->\
+        <ins class="adsbygoogle"\
+             style="display:block"\
+             data-ad-client="ca-pub-7975270895217217"\
+             data-ad-slot="4722632936"\
+             data-ad-format="auto"\
+             data-full-width-responsive="true"></ins>\
+        <s'+'c'+'r'+'i'+'p'+'t>\
+             (adsbygoogle = window.adsbygoogle || []).push({});\
+        </s'+'c'+'r'+'i'+'p'+'t>';
       }
     }
 
