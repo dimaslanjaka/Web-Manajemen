fab74ea4dba036190bfd10a134bca5402253f46e
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-01-08 19:21:07 +0700

    fix

--- WMI/pages/builder.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/dom.php';
-libxml_use_internal_errors(true);
-$xml = file_get_contents(__DIR__ . "/../theme.xml");
-$html = str_get_html($xml);
-foreach ($html->find('b:skin,b:template-skin') as $skin) {
-  $skin->outertext = '<style>' . $skin->innertext . '</style>';
-}
-echo $html->find('html', 0)->outertext;
