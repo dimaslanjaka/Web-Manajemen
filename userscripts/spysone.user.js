1799ecfe608da6350ee494521eb02cfb126a8c62
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-01-13 22:32:28 +0700

    fix

--- userscripts/spysone.user.js
+++ userscripts/spysone.user.js
@@ -8,8 +8,8 @@
 // @match        http://spys.one/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
-// @updateURL    https://rawgit.com/dimaslanjaka/Web-Manajemen/raw/master/userscripts/spysone.user.js
-// @downloadURL  https://rawgit.com/dimaslanjaka/Web-Manajemen/raw/master/userscripts/spysone.user.js
+// @updateURL    https://github.com/dimaslanjaka/Web-Manajemen/raw/master/userscripts/spysone.user.js
+// @downloadURL  https://github.com/dimaslanjaka/Web-Manajemen/raw/master/userscripts/spysone.user.js
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAACVBMVEUAAGcAAABmzDNZt9VtAAAAAXRSTlMAQObYZgAAAF5JREFUeNrtkTESABAQxPD/R6tsE2dUGYUtFJvLDKf93KevHJAjpBorAQWSBIKqFASC4G0pCAkm4GfaEvgYXl0T6HBaE97f0vmnfYHbZOMLZCx9ISdKWwjOWZSC8GYm4SUGwfYgqI4AAAAASUVORK5CYII=
 
 // ==/UserScript==
