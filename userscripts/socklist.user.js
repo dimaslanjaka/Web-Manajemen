28c0b24462bd537af20f7cf384e1775706bd5869
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-01-13 23:18:34 +0700

    fix mixed contents

--- userscripts/socklist.user.js
+++ userscripts/socklist.user.js
@@ -9,7 +9,7 @@
 // @match        http://proxyhttp.net/*
 // @match        http://sockslist.net/*
 // @grant        none
-// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
 // @updateURL    https://rawgit.com/dimaslanjaka/Web-Manajemen/master/userscripts/socklist.user.js
 // @downloadURL  https://rawgit.com/dimaslanjaka/Web-Manajemen/master/userscripts/socklist.user.js
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAACVBMVEUAAGcAAABmzDNZt9VtAAAAAXRSTlMAQObYZgAAAF5JREFUeNrtkTESABAQxPD/R6tsE2dUGYUtFJvLDKf93KevHJAjpBorAQWSBIKqFASC4G0pCAkm4GfaEvgYXl0T6HBaE97f0vmnfYHbZOMLZCx9ISdKWwjOWZSC8GYm4SUGwfYgqI4AAAAASUVORK5CYII=
