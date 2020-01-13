d62552c1ced17c543cc141d3615e13fe88066740
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-01-13 21:53:20 +0700

    fix

---
+++ userscripts/socklist.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Socklist.net proxy grabber
+// @namespace    http://web-manajemen.blogspot.com/
+// @version      1.0
+// @description  try to grabbing proxies from Socklist.net
+// @author       Dimas Lanjaka <dimaslanjaka[at]gmail.com>
+// @match        https://sockslist.net//*
+// @match        http://sockslist.net/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// @updateURL    https://rawgit.com/dimaslanjaka/Web-Manajemen/raw/master/userscripts/spysone.user.js
+// @downloadURL  https://rawgit.com/dimaslanjaka/Web-Manajemen/raw/master/userscripts/spysone.user.js
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAACVBMVEUAAGcAAABmzDNZt9VtAAAAAXRSTlMAQObYZgAAAF5JREFUeNrtkTESABAQxPD/R6tsE2dUGYUtFJvLDKf93KevHJAjpBorAQWSBIKqFASC4G0pCAkm4GfaEvgYXl0T6HBaE97f0vmnfYHbZOMLZCx9ISdKWwjOWZSC8GYm4SUGwfYgqI4AAAAASUVORK5CYII=
+// ==/UserScript==
+
+(function() {
+  'use strict';
+   var t = document.querySelector('table.proxytbl');
+   console.log(t)
+})();
