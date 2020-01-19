bb3498413afff713628bdb8b850dbe2f575aa4a3
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-01-20 02:59:11 +0700

    fix try

--- js/websocket.js
+++ js/websocket.js
@@ -3,7 +3,7 @@
  * @todo Live Data
  * @description Don't miss data that changes even for a second
  * @author Dimas Lanjaka <dimaslanjaka[at]gmail.com
- * @see https://www.webmanajemen.com/2020/01/simple-websocket.html Simple Web Socket
+ * @see https://www.webmanajemen.com/p/simple-websocket.html Simple Web Socket
  */
 var socket = false;
 
