a4247a2a7b88e31c2ca98ccb8469641c8a9e8645
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-01-21 00:36:52 +0700

    added load css async

---
+++ js/loadcss.js
@@ -0,0 +1,18 @@
+/**
+ * Load CSS asynchronously
+ * @param string e css URL
+ * @param HTMLScriptElement t
+ * @param string n media css
+ */
+function loadCSS(e, t, n) {
+  "use strict";
+  var i = window.document.createElement("link");
+  var o = t || window.document.getElementsByTagName("script")[0];
+  i.rel = "stylesheet";
+  i.href = e;
+  i.media = "only x";
+  o.parentNode.insertBefore(i, o);
+  setTimeout(function() {
+    i.media = n || "all"
+  });
+}
