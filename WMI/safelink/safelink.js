c08e08b2aa48b57662330a4ed63b8e397da076bd
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-01-21 10:27:34 +0700

    fix

--- WMI/safelink/safelink.js
+++ WMI/safelink/safelink.js
@@ -1,9 +1,12 @@
-var x = Up("url") || Up("u"), safelinkURL = 'https://dimaslanjaka.github.io/page/safelink.html?url=';
-if (location.host != 'translate.googleusercontent.com' && x && "" != x) {
-  location.replace(safelinkURL + x);
+var x = parse_query("url") || parse_query("u");
+if (typeof safelinkURL == 'undefined'){
+  var safelinkURL = sfUrl();
+}
+if (location.host != 'translate.googleusercontent.com' && x) {
+  location.href = safelinkURL + x;
 }
 
-var EXCLUDE = /facebook\.com|twitter\.com|thumblr\.com|blog\.akarmas\.com|web\-manajemen\.blogspot\.com|dimaslanjaka\.github\.io|webmanajemen\.com|www\.blogger\.com|translate\.google\.com|translate\.googleusercontent\.com|javascript\:|\#|dimaslanjaka/gm,
+var EXCLUDE = /facebook\.com|twitter\.com|thumblr\.com|blog\.akarmas\.com|web\-manajemen\.blogspot\.com|dimaslanjaka\.github\.io|webmanajemen\.com|www\.blogger\.com|translate\.google\.com|translate\.googleusercontent\.com|javascript\:|\#/gm,
   l = document.addEventListener || document.attachEvent;
 l("mouseover", function(e) {
   if ("a" == (e.target.nodeName || e.srcElement.nodeName).toLowerCase()) {
@@ -12,7 +15,8 @@ l("mouseover", function(e) {
     if (e.target.host.match(/javascript\:void/gm)) {
       return;
     }
-    if (!a.match(EXCLUDE)) {
+    host = parse_url(a).hostname;
+    if (!host.match(EXCLUDE)) {
       t.href = safelinkURL + btoa(a);
       t.setAttribute('target', '_blank');
       if (typeof t.classList != 'undefined') {
@@ -20,32 +24,50 @@ l("mouseover", function(e) {
       } else {
         t.setAttribute('class', 'ext-link');
       }
-    }
+    } else if (host == 'translate.googleusercontent.com') {}
   }
 });
-/*
-l("mouseover", function (e) {
-if ("a" == (e.target.nodeName || e.srcElement.nodeName).toLowerCase()) {
-var t = e.target,
-a = t.href;
-t.getAttribute("data-href") || a.match(EXCLUDE) || (t.setAttribute("data-href", a), t.removeAttribute("href"), t.style.textDecoration = "underline", t.style.color = "red")
-}
-});
 
-l("click", function (e) {
-if ("a" == (e.target.nodeName || e.srcElement.nodeName).toLowerCase()) {
-e.preventDefault();
-console.log(e);
-var t = e.target.href || e.target.getAttribute("data-href");
-return (e.target.host || t).match(EXCLUDE) ? location.href = t : Object.assign(document.createElement("a"), {
-target: "_blank",
-href: "https://dimaslanjaka.github.io/page/safelink.html?url=" + btoa(t)
-}).click(), !1
+/**
+ * Get safelink URL
+ */
+function sfUrl() {
+  return 'https://dimaslanjaka.github.io/page/safelink.html?ID=' + Math.floor(Math.random()) + '&url=';
 }
-});
-*/
-function Up(e, a) {
-  a || (a = window.location.href), e = e.replace(/[\[\]]/g, "\\$&");
-  var l = new RegExp("[?&]" + e + "(=([^&#]*)|&|#|$)").exec(a);
-  return l ? l[2] ? decodeURIComponent(l[2].replace(/\+/g, " ")) : "" : null
+
+/**
+ * parse url to object
+ * @param string href
+ * @return HTMLAnchorElement
+ * @return Object.query
+ */
+function parse_url(href) {
+  if (!href) {
+    href = location.href;
+  }
+  var l = document.createElement("a");
+  l.href = href;
+  l.query = parse_query({}, href);
+  return l;
+};
+/**
+ * Parse Query URL and Hash
+ * @param string query
+ * @param string url
+ */
+function parse_query(query, url) {
+  url || (url = window.location.search);
+  var urlParams = new URLSearchParams(url);
+  var urlp = Object.fromEntries(urlParams);
+  var hash = window.location.hash.substr(1);
+  var urlParams = new URLSearchParams(hash);
+  var urlh = Object.fromEntries(urlParams);
+  var urlO = Object.assign(urlh, urlp);
+  if (typeof query == 'function') {
+    return urlO;
+  }
+  if (query && urlO.hasOwnProperty(query)) {
+    return urlO[query];
+  }
+  return false;
 }
