940dc308571c380d0c6cabe03d2b60e0865d9353
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-01-22 12:37:51 +0700

    fix

--- js/adsense.js
+++ js/adsense.js
@@ -1,13 +1,28 @@
+/**
+ * Lazy load Adsense with customizable config
+ * @var boolean alertAds show alert ads every page loaded with sweetalert. default true
+ * @see https://sweetalert.js.org/docs/#content SweetAlert
+ * @var string alertAds_html adsense HTML or other ads HTML string. default string
+ * @var boolean VerifyLoadedAds verify adsense was loaded or not. default false
+ */
+if (typeof alertAds == 'undefined') {
+  var alertAds = true;
+  if (typeof alertAds_html == 'undefined') {
+    var alertAds_html = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7975270895217217" data-ad-slot="4894289831" data-ad-format="link" data-full-width-responsive="true"></ins>';
+  }
+}
+if (typeof VerifyLoadedAds == 'undefined') {
+  var VerifyLoadedAds = false;
+}
 (function () {
-  if (typeof swal != 'undefined') {
-    var ads_h = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7975270895217217" data-ad-slot="4894289831" data-ad-format="link" data-full-width-responsive="true"></ins>';
+  if (typeof swal != 'undefined' && alertAds) {
     swal({
       title: 'Donate With Click ^_^',
       /*type: 'info',*/
       content: {
         element: 'div',
         attributes: {
-          innerHTML: ads_h + '<li class="fas fa-link"></i> <a href="//web-manajemen.blogspot.com"><b>Website Development Indonesia</b></a> '
+          innerHTML: alertAds_html + '<li class="fas fa-link"></i> <a href="//web-manajemen.blogspot.com"><b>Website Development Indonesia</b></a> '
         }
       },
       buttons: {
@@ -30,7 +45,7 @@
       closeOnClickOutside: false
     });
   }
-  if (typeof RegexHost == 'undefined'){
+  if (typeof RegexHost == 'undefined') {
     /**
      * @var RegExp RegexHost regex matching for exclude from adsense
      */
@@ -50,7 +65,9 @@
       });
       $('.adsbygoogle').each(function (i, obj) {
         if (!obj.hasAttribute('pushed')) {
-          //if (obj.getElementsByTagName('iframe').length) return;
+          if (typeof VerifyLoadedAds != 'undefined' && VerifyLoadedAds) {
+            if (obj.getElementsByTagName('iframe').length) return;
+          }
           (adsbygoogle = window.adsbygoogle || []).push({});
           obj.setAttribute('pushed', 'true');
         }
