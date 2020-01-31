c0a97b7fbf276c0343fda1ad27c3adc2e5f17750
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-01-31 09:30:47 +0700

    fix webhook

--- userscripts/freeproxylist.user.js
+++ userscripts/freeproxylist.user.js
@@ -48,7 +48,7 @@ function parsingFPL() {
 
 function checkSPL() {
   if (typeof global_proxies[global_index] != 'undefined') {
-    $.post('https://agcontents.000webhostapp.com/proxy-receiver.php', { save: global_proxies[global_index] }).always(function () {
+    $.post('https://wp.webmanajemen.com/receiver/proxy-receiver.php', { save: global_proxies[global_index] }).always(function () {
       global_index++;
       if (global_index < global_proxies.length) {
         checkSPL();
