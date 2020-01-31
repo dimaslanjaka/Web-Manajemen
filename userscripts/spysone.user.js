46c9b832e075f16cf4c588e6a2485fe887a86caf
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-01-31 09:30:47 +0700

    fix webhook

--- userscripts/spysone.user.js
+++ userscripts/spysone.user.js
@@ -33,7 +33,7 @@ var global_index = 0, global_proxies = [];
 
 function checkSpys() {
     if (typeof global_proxies[global_index] != 'undefined') {
-        $.post('https://agcontents.000webhostapp.com/proxy-receiver.php', { save: global_proxies[global_index] }).always(function () {
+        $.post('https://wp.webmanajemen.com/receiver/proxy-receiver.php', { save: global_proxies[global_index] }).always(function () {
             global_index++;
             if (global_index < global_proxies.length) {
                 checkSpys();
