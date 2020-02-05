cb20e0fa614783cc1060f2f68b7ed1e7df9343ee
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-02-05 21:40:32 +0700

    data dump fix

--- docs/select2/js/app/paginate.js
+++ docs/select2/js/app/paginate.js
@@ -1,7 +1,7 @@
 define(["jquery", "select2", "loadcss"], function ($) {
     $(function () {
         loadCSS(['https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css', './css/style.min.css?v=11', './css/select2.min.css']);
-        /** Declare global data */
+        /** Declare global dump data */
         var items = [];
         for (var i = 0; i < 10000; i++) {
             items.push({ id: i, text: "item " + i })
@@ -10,7 +10,6 @@ define(["jquery", "select2", "loadcss"], function ($) {
         pageSize = 50
 
         $.fn.select2.amd.require(["select2/data/array", "select2/utils"],
-
             function (ArrayData, Utils) {
                 function CDATA($element, options) {
                     CDATA.__super__.constructor.call(this, $element, options);
