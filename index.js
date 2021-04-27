c84c1567dd922e83509ab7e3695fe5b3dbd40a60
dimaslanjaka <dimaslanjaka@gmail.com>
2021-04-27 20:36:29 +0700

    preview builder php

--- index.js
+++ index.js
@@ -13,13 +13,18 @@ for (var key in LISTS) {
 	var files = LISTS[key];
 
 	var ulfolder = document.createElement("li");
-	ulfolder.textContent = key;
+	ulfolder.textContent = key.replace(/^\/build/s, "");
 	var ulfiles = document.createElement("ul");
 	for (let index = 0; index < files.length; index++) {
+		/**
+		 * @type {string}
+		 */
 		var file = files[index];
 		var ulfile = document.createElement("li");
 		ulfile.id = "child_node_" + (index + 1);
-		ulfile.textContent = file;
+		ulfile.textContent = getFileName(file);
+		ulfile.setAttribute("href", `/index.php?render=${file}`);
+		ulfile.setAttribute("data-jstree", json_encode({ type: "file" }));
 
 		// add files to ul folders
 		ulfiles.appendChild(ulfile);
@@ -34,16 +39,49 @@ for (var key in LISTS) {
 var jstre = document.getElementById("jstree");
 jstre.appendChild(ul);
 
+function json_encode(obj) {
+	return JSON.stringify(obj);
+}
+
+function getFileName(fullPath) {
+	var startIndex =
+		fullPath.indexOf("\\") >= 0
+			? fullPath.lastIndexOf("\\")
+			: fullPath.lastIndexOf("/");
+	var filename = fullPath.substring(startIndex);
+	if (filename.indexOf("\\") === 0 || filename.indexOf("/") === 0) {
+		filename = filename.substring(1);
+	}
+	return filename;
+}
+
 // initialize JsTree
 $(function () {
 	// 6 create an instance when the DOM is ready
-	$("#jstree").jstree();
+	$("#jstree")
+		.jstree({
+			plugins: ["themes", "html_data", "ui", "crrm", "types"],
+			types: {
+				folder: {
+					icon: "far fa-folder",
+				},
+				file: {
+					icon: "far fa-file",
+				},
+				default: {
+					icon: "far fa-folder",
+				},
+			},
+		})
+		.bind("select_node.jstree", function (event, data) {});
 	// 7 bind to events triggered on the tree
 	$("#jstree").on("changed.jstree", function (e, data) {
-		console.log(data.selected);
+		if (typeof data.node.li_attr.href == "string") {
+			location.href = data.node.li_attr.href;
+		}
 	});
 	// 8 interact with the tree - either way is OK
-	$("button").on("click", function () {
+	$("#button").on("click", function () {
 		$("#jstree").jstree(true).select_node("child_node_1");
 		$("#jstree").jstree("select_node", "child_node_1");
 		$.jstree.reference("#jstree").select_node("child_node_1");
--- index.php
+++ index.php
@@ -7,6 +7,17 @@ header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', FALSE);
 header('Pragma: no-cache');
 
+// RENDER FILE
+if (isset($_GET['render'])) {
+  $file = __DIR__ . '/' . $_GET['render'];
+  if (file_exists($file)) {
+    include($file);
+    exit;
+  }
+}
+
+/// LIST DIRECTORY
+
 $list = getDirContents(__DIR__ . '/build');
 // sort list by length ()
 $keys = array_map('strlen', array_values($list));
@@ -33,15 +44,18 @@ $list = array_map('path2web', $list);
 //print_r($list);
 ?>
 
-<main class="container">
-  <button>demo button</button>
+<main class="container mt-4 mb-4">
+  <code id="button">build/</code>
   <div id="jstree"></div>
 </main>
 
-
+<link rel="stylesheet" href="https://raw.githack.com/dimaslanjaka/Web-Manajemen/master/css/all.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/themes/default/style.min.css'>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/jstree.min.js'></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
 <script>
   const list = <?= json_encode($list) ?>;
   const listbuild = <?= json_encode($listbuild) ?>;
