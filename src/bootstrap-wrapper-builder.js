85acf70da8b30bb77b5d0e70e79340aa262681d0
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-05 11:07:35 +0700

    chore: fresh build

--- src/bootstrap-wrapper-builder.js
+++ src/bootstrap-wrapper-builder.js
@@ -1,14 +1,15 @@
-// clone https://github.com/twbs/bootstrap.git tmp/bootstrap
-// cd tmp/bootstrap && npm install
+// clone https://github.com/twbs/bootstrap.git /tmp/bootstrap
+// cd /tmp/bootstrap && npm install
+
 const sass = require("sass");
 const path = require("path");
 
-const pkgtwbs = require("./tmp/bootstrap/package.json");
+const pkgtwbs = require("../tmp/bootstrap/package.json");
 const { writefile } = require("sbg-utility");
 const version = pkgtwbs.version.replace(/\./gm, "-");
 const filename = "bootstrap-wrapper-" + version + ".css";
 const result = sass.compile(
 	path.join(__dirname, "bootstrap-wrapper-builder.scss")
 );
-const saveto = path.join(__dirname, "css", filename);
+const saveto = path.join(__dirname, "../css", filename);
 writefile(saveto, result.css);
