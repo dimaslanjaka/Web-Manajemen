502f0c5462dd493769d1d5ed0414ecc2c9124294
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-08 19:56:23 +0700

    clean repo

--- docs/select2/readme.md
+++
@@ -1,8 +0,0 @@
-## Installation
-composer install
-
-## usage
-
-php -S localhost:8000
-
-access localhost:8000 in your browser
--- git.sh
+++
@@ -1,7 +0,0 @@
-git init
-git commit -m "Re-Build"
-git remote add origin https://github.com/dimaslanjaka/Web-Manajemen.git
-
-git remote set-url origin https://github.com/dimaslanjaka/Web-Manajemen.git
-
-git push -u origin master
