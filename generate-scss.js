526d17cec9b3fa41f095f87438b7a766d30c5bcf
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-14 10:58:48 +0700

    ignore only file on `fontawesome.scss`

--- generate-scss.js
+++ generate-scss.js
@@ -3,16 +3,18 @@ const path = require('path')
 
 const base = path.resolve(__dirname, 'scss')
 let scss = `
+// https://fontawesome.com/docs/web/use-with/scss
 // importing core styling file
 @import "./fontawesome.scss";
 
 // import all
 `
-
+const rgIgnore = /^_(functions|variables|mixins|core|sizing|fixed-width|list|bordered-pulled|animated|rotated-flipped|stacked|icons|screen-reader).scss$/
 scss += fs.readdirSync(base)
-  .filter(file => !file.startsWith('_'))
-  .filter(file => !['all.scss', 'fontawesome.scss'].includes(file))
+  //.filter(file => !file.startsWith('_'))
+  .filter(file => !['all.scss', 'fontawesome.scss'].includes(file) && !rgIgnore.test(file))
   .map(file => {
+    console.log(file, rgIgnore.test(file))
     return `@import "./${file}";`
   }).join('\n')
 
