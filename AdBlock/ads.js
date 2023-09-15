cef46a50b5493fdf4952838be793536741eee562
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-16 00:43:35 +0700

    remove unused files

--- AdBlock/ads.js
+++
@@ -1,4 +0,0 @@
-var e=document.createElement('div');
-e.id='EiBVMgCWjUGo';
-e.style.display='none';
-document.body.appendChild(e);
--- templates/pinktong/adsense.js
+++
@@ -1,8 +0,0 @@
-if (typeof dimas != 'undefined') {
-  dimas.js('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', function(){
-    var ins = document.getElementsByTagName('ins').length;
-    for (let index = 0; index < ins; index++) {
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    }
-  });
-}
